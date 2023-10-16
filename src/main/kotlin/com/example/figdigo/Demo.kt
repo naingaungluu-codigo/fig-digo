534dbda7222a7d4ae7120d47caea2c3b461ac233
Zay Yar Pyae Phyoe <91115259+zayyarpyaephyoe-codigo@users.noreply.github.com>
2023-10-16 11:35:51 +0630

    wip

---
+++ src/main/kotlin/com/example/figdigo/Demo.kt
@@ -0,0 +1,10 @@
+package com.example.figdigo
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class Demo : AnAction(){
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
